036f692a35af26f30559f903e03b5bce659c7ac6
ronald vilorio <soccer23415@gmail.com>
2018-01-13 21:55:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,8 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      @@cart.each do |item
+      @@cart.each do |item|
+        resp.write "#{item}"
 
     else
       resp.write "Path Not Found"
