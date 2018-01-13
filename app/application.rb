69fcc22d639d30b9d3a41a93027414b0de566a5f
ronald vilorio <soccer23415@gmail.com>
2018-01-13 22:20:54 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -27,6 +27,7 @@ class Application
       search_term = req.params["q"]
       if @@items.include?(search_term)
         @@cart << search_term
+        "added #{search_term}"
 
     else
       resp.write "Path Not Found"
