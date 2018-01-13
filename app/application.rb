e33449c804c5ba281912e2abfad142f89a5cd6f3
ronald vilorio <soccer23415@gmail.com>
2018-01-13 22:10:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -22,7 +22,7 @@ class Application
       end
     elsif req.path.match(/add/)
       @@items.each do |item|
-        @@cart << item
+        @@cart << item if item
 
     else
       resp.write "Path Not Found"
